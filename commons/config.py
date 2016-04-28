d8eee27a9a9f02d5834418de03cb64e0b1d96764
dujinle <yingyongdujinle@163.com>
2016-04-28 10:19:16 +0000

    update config.py

--- commons/config.py
+++ commons/config.py
@@ -17,15 +17,16 @@ dfiles = {
 		"10":os.path.join(abspath,'data','voice','Num.txt')
 	},
 	'Temp':{
-		"1":os.path.join(abspath,'data','voice','M.txt'),
-		"2":os.path.join(abspath,'data','voice','C.txt'),
-		"3":os.path.join(abspath,'data','voice','F.txt'),
-		"4":os.path.join(abspath,'data','voice','X.txt'),
-		"5":os.path.join(abspath,'data','voice','X1.txt'),
-		"6":os.path.join(abspath,'data','voice','M1.txt'),
-		"7":os.path.join(abspath,'data','voice','F1.txt'),
-		"8":os.path.join(abspath,'data','voice','Z.txt'),
-		"9":os.path.join(abspath,'data','voice','PM.txt'),
-		"10":os.path.join(abspath,'data','voice','Num.txt')
+		"1":os.path.join(abspath,'data','temperature','M.txt'),
+		"2":os.path.join(abspath,'data','temperature','C.txt'),
+		"3":os.path.join(abspath,'data','temperature','F.txt'),
+		"4":os.path.join(abspath,'data','temperature','X.txt'),
+		"5":os.path.join(abspath,'data','temperature','Nt.txt'),
+		"6":os.path.join(abspath,'data','temperature','X1.txt'),
+		"7":os.path.join(abspath,'data','temperature','M1.txt'),
+		"8":os.path.join(abspath,'data','temperature','F1.txt'),
+		"9":os.path.join(abspath,'data','temperature','Z.txt'),
+		"10":os.path.join(abspath,'data','temperature','PM.txt'),
+		"11":os.path.join(abspath,'data','temperature','Num.txt')
 	}
 };
