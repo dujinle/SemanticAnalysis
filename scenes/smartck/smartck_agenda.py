0773ca6fd86dd69e81064524614da73ddae27baa
dujinle <yingyongdujinle@163.com>
2016-05-03 13:38:55 +0800

    update for agenda

--- scenes/smartck/smartck_agenda.py
+++ scenes/smartck/smartck_agenda.py
@@ -1,43 +1,58 @@
 #!/usr/bin/python
 #-*- coding:utf-8 -*-
-import sys,os,json,copy
-import re,time,common
+import sys,os,json
+import re
+import common,math
 from common import logging
 from myexception import MyException
 from scene_base import SceneBase
-import com_funcs as SceneParam
 import smartck_common as SmartckCom
+import com_funcs as SceneParam
 
-#添加闹钟场景 或者带有时间
-class SmartckAdd(SceneBase):
+#新增日程闹钟 或者带有时间的
+class SmartckAgenda(SceneBase):
 
 	def encode(self,struct,super_b):
-		if not struct.has_key('ck_scene'): return None;
-		if struct['ck_scene'] <> 'scene_add': return None;
 		try:
-			print 'go into scene add action......';
-			if not struct.has_key('step'): struct['step'] = 'start';
+			if not struct.has_key('ck_scene'): return None;
+			if struct['ck_scene'] <> 'ck_agenda_add': return None;
 
+			print 'go into scene agenda......';
 			SmartckCom._fetch_time(struct);
 			SmartckCom._calc_able(struct);
-			#启动时响应回复
+			if not struct.has_key('step'): struct['step'] = 'start';
 			if struct['step'] == 'start':
 				super_b.myclock = dict();
-				#开始设置必须的参数
-				self.set_ck_param(struct);
+				self._set_agenda_info(struct,super_b);
 				return None;
+			elif struct['step'] == 'trans':
+				self._set_clock_info(struct,super_b);
 			elif struct['step'] == 'set_time':
-				self.set_ck_param(struct);
+				self._set_agenda_info(struct,super_b);
 				return None;
-			struct['step'] = 'trans';
+			struct['step'] = 'end';
 		except Exception as e:
 			raise MyException(sys.exc_info());
 
-	def set_ck_param(self,struct,super_b):
+	def _set_clock_info(self,struct,super_b):
+		if super_b.myclock.has_key('name'):
+			super_b.myclock['key'] = super_b.myclock['name'];
+			super_b.clocks[super_b.myclock['key']] = super_b.myclock;
+			SceneParam._set_msg(self.data['msg']['set_succ']);
+		elif super_b.myclock.has_key('info'):
+			super_b.clocks[super_b.myclock['info']] = super_b.myclock;
+			super_b.myclock['key'] = super_b.myclock['info'];
+			SceneParam._set_msg(self.data['msg']['set_succ'],super_b.myclock['info']);
+		else:
+			super_b.clocks[super_b.myclock['time']] = super_b.myclock;
+			super_b.myclock['key'] = super_b.myclock['time'];
+			SceneParam._set_msg(self.data['msg']['set_succ']);
+
+	def _set_agenda_info(self,struct,super_b):
 		self._set_time(struct,super_b);
 		self._set_name(struct,super_b);
 		self._set_able(struct,super_b);
-		self._check_param(super_b);
+		self._check_param(struct,super_b);
 
 	def _set_time(self,struct,super_b):
 		myclock = super_b.myclock;
@@ -47,12 +62,10 @@ class SmartckAdd(SceneBase):
 			if hour >= self.data['common']['getup_time'][0] \
 				and hour <= self.data['common']['getup_time'][1]:
 				myclock['type'] = 'getup';
-				struct['ck_scene'] = 'ck_getup_add';
 				#时间设置完成回应信息
 			else:
 				SceneParam._set_msg(self.data['msg']['add_getup_ck']);
 				myclock['type'] = 'agenda';
-				struct['ck_scene'] = 'ck_agenda_add'
 				#时间设置完成回应信息
 			myclock['time'] = struct['ck_time']['time'];
 			del struct['ck_time'];
@@ -78,5 +91,6 @@ class SmartckAdd(SceneBase):
 			SceneParam._set_msg(struct,self.data['msg']['set_time']);
 			struct['step'] = 'set_time';
 		else:
-			struct['step'] = 'trans';
+			self._set_clock_info(struct,super_b);
+			struct['step'] = 'end';
 
