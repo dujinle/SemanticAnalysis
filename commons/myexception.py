96c5a38fd378eb06fe4a40f582465a3c676fa9d1
dujinle <yingyongdujinle@163.com>
2016-05-07 07:26:46 +0800

    update myexception

--- commons/myexception.py
+++ commons/myexception.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python
 #-*- coding:utf-8 -*-
-import json
-CODENUM = 0;
+import traceback
 class MyException(Exception):
 
-	def __init__(self,value):
-		self.value = value;
+	def __init__(self,info):
+		self.etype = info[0];
+		self.msg = info[1];
+		self.trace = traceback.extract_tb(info[2]);
 
 	def __str__(self):
-		vtype = type(self.value);
-		res = self.value;
-		global CODENUM;
-		CODENUM = CODENUM + 1;
-		if vtype == dict or vtype == list:
-			res = json.dumps(self.value,indent = 2,ensure_ascii = False);
-		return str('CodeError[' + str(CODENUM) + ']:' + res);
+		infos = list();
+		infos.append('Traceback (most recent call last):');
+		for trace in self.trace:
+			allinfo = '  File' + ' \"' + str(trace[0]) + '\",'\
+					+ ' line ' + str(trace[1]) + ', in <' + str(trace[2]) + '>';
+			infos.append(allinfo);
+			allinfo = '    ' + str(trace[3]);
+			infos.append(allinfo);
+		stype = str(self.etype.__name__) + ': ' + str(self.msg);
+		infos.append(stype)
+		return '\n'.join(infos);
