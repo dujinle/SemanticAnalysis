d23a39095b549a19d413d26e8f227e8ecd24bf72
dujinle <yingyongdujinle@163.com>
2016-06-17 06:12:53 +0000

    support local cell

---
+++ mainpy/location/AD.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+import sys,os
+reload(sys);
+sys.setdefaultencoding('utf-8');
+import common,config,localmm
+from myexception import MyException
+class AD:
+	def __init__(self):
+		self.q = dict();
+		self.x = dict();
+		self.c = dict();
+		self.l = dict();
+		self.poi = dict();
+
+	def init(self,fdir):
+		try:
+			self.q = localmm.readfile(fdir + '/BJHDQ.txt');
+			self.x = localmm.readfile(fdir + '/BJHDX.txt');
+			self.c = localmm.readfile(fdir + '/BJHDC.txt');
+			self.s = localmm.readfile(fdir + '/BJHDS.txt');
+			self.p = localmm.readfile(fdir + '/BJHDP.txt');
+		except MyException as e:
+			raise e;
+
+	def encode(self,struct):
+		self._paser_addr(struct);
+		self._paser_poi(struct);
+		self._paser_adcode(struct);
+		self._calc_them(struct);
+
+	def _paser_addr(self,struct):
+		inlist = struct['inlist'];
+		taglist = struct['locals'];
+		for instr in taglist:
+			if type(instr) == dict: continue;
+			tdic = dict();
+			tdic['type'] = 'local'
+			tdic['value'] = instr;
+			idx = taglist.index(instr);
+			if self.q.has_key(instr):
+				tdic['scope'] = 'q';
+				taglist[idx] = tdic;
+			elif self.x.has_key(instr):
+				tdic['scope'] = 'x';
+				taglist[idx] = tdic;
+			elif self.c.has_key(instr):
+				tdic['scope'] = 'c';
+				taglist[idx] = tdic;
+
+	def _paser_poi(self,struct):
+		inlist = struct['inlist'];
+		taglist = struct['locals'];
+		for instr in inlist:
+			tdic = dict();
+			tdic['type'] = 'local';
+			tdic['value'] = instr;
+			if self.p.has_key(instr):
+				tdic['scope'] = 'p';
+				taglist.append(tdic);
+
+	def _paser_adcode(self,struct):
+		inlist = struct['inlist'];
+		taglist = struct['locals'];
+		idx = 0
+		while True:
+			if idx >= len(taglist): break;
+			instr = taglist[idx];
+			if type(instr) == dict:
+				idx = idx + 1;
+				continue;
+			if self.s.has_key(instr):
+				tdic = dict();
+				tdic['type'] = 'local';
+				tdic['scope'] = 'l';
+				if idx < len(taglist) - 2:
+					if taglist[idx + 2] == u'号':
+						instr = instr + taglist[idx + 1] + taglist[idx + 2];
+						del taglist[idx + 1];
+						del taglist[idx + 1];
+				tdic['value'] = instr;
+				taglist[idx] = tdic;
+			idx = idx + 1;
+
+	def _calc_them(self,struct):
+		taglist = struct['locals'];
+		tdic = None;
+		idx = firstidx = 0;
+		flag = False;
+		while True:
+			if idx >= len(taglist): break;
+			tag = taglist[idx];
+			if type(tag) == dict and tag['type'] == 'local':
+				scope = tag['scope'];
+				if flag == False:
+					flag = True;
+					tdic = dict();
+					tdic['type'] = 'locals';
+					tdic[scope] = tag['value'];
+					firstidx = idx;
+				elif flag == True:
+					tdic[scope] = tag['value'];
+					del taglist[idx];
+					idx = idx - 1;
+			else:
+				if not tdic is None:
+					taglist[firstidx] = tdic;
+					flag = False;
+					tdic = None;
+			idx = idx + 1;
+		if flag == True:
+			taglist[firstidx] = tdic;
+
---
+++ mainpy/location/CR.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+import sys,os,re
+reload(sys);
+sys.setdefaultencoding('utf-8');
+import common,config,localmm
+from myexception import MyException
+class CR:
+	def __init__(self):
+		self.reg = u'L[与和]{1}L[的]*交叉[路]*口';
+
+	def init(self,fdir):
+		pass;
+
+	def encode(self,struct):
+		self._calc_them(struct);
+
+	def _calc_them(self,struct):
+		taglist = struct['locals'];
+		strs = '';
+		for tag in taglist:
+			if type(tag) == dict and tag['type'] == 'locals':
+				strs = strs + 'L';
+			else:
+				strs = strs + tag;
+		comp = re.compile(self.reg);
+		match = comp.search(strs);
+		while True:
+			if match is None: return None;
+			idx = strs.find(match.group(0));
+			first = self._num_tag(strs,idx,taglist);
+			tdic = dict();
+			f1 = taglist[first];
+			yu = taglist[first + 1];
+			f2 = taglist[first + 2];
+			tdic['d1'] = dict(f1);
+			tdic['d2'] = dict(f2);
+			tdic['type'] = 'local_cross';
+			taglist[first] = tdic;
+			del taglist[first + 1];
+			del taglist[first + 1];
+			if taglist[first + 1] == u'的':
+				del taglist[first + 1];
+			del taglist[first + 1];
+			strs = strs.replace(match.group(0),'LL',1);
+			match = comp.search(strs);
+
+	def _num_tag(self,strs,idx,taglist):
+		mystr = strs[:idx];
+		ms = re.findall('L',mystr);
+		num = len(ms);
+		idx = 0
+		while True:
+			if idx >= len(taglist): break;
+			tag = taglist[idx];
+			if type(tag) == dict and tag['type'] == 'locals':
+				if num <= 0: return idx;
+				else: num = num - 1;
+			idx = idx + 1;
+		return idx - 1;
+
