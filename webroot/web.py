1c3f52225685f5190b8a6fbcdbfd7d73b94aed09
dujinle <yingyongdujinle@163.com>
2016-06-29 05:46:15 +0000

    update for flight

--- webroot/web.py
+++ webroot/web.py
@@ -20,6 +20,10 @@ class Application(tornado.web.Application):
 		handlers = [
 			(r"/doctor",DocterHandler),
 			(r"/voice",VoiceHandler),
+			(r"/local",LocalHandler),
+			(r"/music",MusicHandler),
+			(r"/catering",CateringHandler),
+			(r"/travel",TravelHandler),
 			(r"/temperature",TempHandler),
 			(r"/timer",TimerHandler),
 			(r"/get_result",ResultHandler),
@@ -51,6 +55,22 @@ class TempHandler(tornado.web.RequestHandler):
 class TimerHandler(tornado.web.RequestHandler):
 	def get(self):
 		self.render('timer.html');
+
+class LocalHandler(tornado.web.RequestHandler):
+	def get(self):
+		self.render('local.html');
+
+class MusicHandler(tornado.web.RequestHandler):
+	def get(self):
+		self.render('music.html');
+
+class CateringHandler(tornado.web.RequestHandler):
+	def get(self):
+		self.render('catering.html');
+
+class TravelHandler(tornado.web.RequestHandler):
+	def get(self):
+		self.render('travel.html');
 
 if __name__=="__main__":
 
