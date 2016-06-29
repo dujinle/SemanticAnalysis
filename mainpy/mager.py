e9e3e1014801abb8389c3f01005e3cc2aed51163
dujinle <yingyongdujinle@163.com>
2016-06-29 05:45:23 +0000

    update for flight

--- mainpy/mager.py
+++ mainpy/mager.py
@@ -12,6 +12,10 @@ base_path = os.path.dirname(__file__);
 sys.path.append(os.path.join(base_path,'./voice'));
 sys.path.append(os.path.join(base_path,'./temperature'));
 sys.path.append(os.path.join(base_path,'./timer'));
+sys.path.append(os.path.join(base_path,'./location'));
+sys.path.append(os.path.join(base_path,'./flight'));
+sys.path.append(os.path.join(base_path,'./catering'));
+sys.path.append(os.path.join(base_path,'./music'));
 sys.path.append(os.path.join(base_path,'./wordsegs'));
 sys.path.append(os.path.join(base_path,'../commons'));
 #==============================================================
@@ -22,6 +26,10 @@ from myexception import MyException
 from voice_mager import VoiceMager
 from temp_mager import TempMager
 from time_mager import TimeMager
+from local_mager import LocalMager
+from music_mager import MusicMager
+from catering_mager import CateringMager
+from flight_mager import FlightMager
 
 class Mager:
 	def __init__(self):
@@ -30,6 +38,10 @@ class Mager:
 		self.modules['Voice'] = VoiceMager(self.wordseg);
 		self.modules['Temp'] = TempMager(self.wordseg);
 		self.modules['Timer'] = TimeMager(self.wordseg);
+		self.modules['Local'] = LocalMager(self.wordseg);
+		self.modules['Music'] = MusicMager(self.wordseg);
+		self.modules['Catering'] = CateringMager(self.wordseg);
+		self.modules['Flight'] = FlightMager(self.wordseg);
 
 	def init(self):
 		try:
@@ -86,7 +98,7 @@ try:
 	#mg.write_file();
 	#common.print_dic(mg.encode(u'把声音调大点'));
 	#mg.sp_deal('del',{'value':u'大点'});
-	common.print_dic(mg.encode(u'把声音调到最大','Voice'));
+	common.print_dic(mg.encode(u'明天上午到上地七街的机票','Flight'));
 	#mg.sp_deal('del',{'value':u'静音'});
 	#mg.deal_data('M','add',{'value':u'音'});
 	#common.print_dic(mg.encode(u'静音'));
