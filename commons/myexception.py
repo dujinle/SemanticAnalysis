b0537267baf979603953673b755d18b37d5026f1
dujinle <yingyongdujinle@163.com>
2017-03-12 16:30:01 +0800

    update for web

--- commons/myexception.py
+++ commons/myexception.py
@@ -8,18 +8,21 @@ class MyException(Exception):
 			self.etype = info[0];
 			self.msg = info[1];
 			self.trace = traceback.extract_tb(info[2]);
-		except Exception e:
-			raise MyException(sys.exec_info());
+		except Exception as e:
+			raise e;
 
 	def __str__(self):
-		infos = list();
-		infos.append('Traceback (most recent call last):');
-		for trace in self.trace:
-			allinfo = '  File' + ' \"' + str(trace[0]) + '\",'\
-					+ ' line ' + str(trace[1]) + ', in <' + str(trace[2]) + '>';
-			infos.append(allinfo);
-			allinfo = '    ' + str(trace[3]);
-			infos.append(allinfo);
-		stype = str(self.etype.__name__) + ': ' + str(self.msg);
-		infos.append(stype)
+		try:
+			infos = list();
+			infos.append('Traceback (most recent call last):');
+			for trace in self.trace:
+				allinfo = '  File' + ' \"' + str(trace[0]) + '\",'\
+						+ ' line ' + str(trace[1]) + ', in <' + str(trace[2]) + '>';
+				infos.append(allinfo);
+				allinfo = '    ' + str(trace[3]);
+				infos.append(allinfo);
+			stype = str(self.etype.__name__) + ': ' + str(self.msg);
+			infos.append(stype)
+		except Exception as e:
+			raise e;
 		return '\n'.join(infos);
